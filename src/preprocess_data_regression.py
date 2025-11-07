a6eb8a49af7a1fa4a106a6f6f413a1b5970eb474
Amal Sebastian <sebast64@msu.edu>
2025-11-07 16:09:05 -0500

    added preprocess_data_regression.py, train_cnn_temp_reg.py, history_reg.ipynb, regression_test.ipynb to do regression on the data set. Everything to do with regression ends with _reg both in src/ , notebooks/ and /results.

---
+++ src/preprocess_data_regression.py
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+import numpy as np
+import glob
+from sklearn.model_selection import train_test_split
+import os 
+
+
+
+# Change directory to the location of this script
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
+
+
+def load_and_prepare_reg_data(data_dir="../data/",L=32,T_c=2.69):
+    """
+    Load and preprocess 2D Ising model configurations for phase classification.
+
+    Parameters
+    ----------
+    data_dir : str
+        Directory containing .npy configuration files.
+    L : int
+        Linear lattice size.
+    T_c : float
+        Critical temperature separating ordered and disordered phases.
+
+    Returns
+    -------
+    X_train, X_val, X_test : np.ndarray
+        Normalized lattice configurations split for training, validation, and testing.
+    y_train, y_val, y_test : np.ndarray
+        Corresponding binary phase labels (0 = ordered, 1 = disordered).
+    """
+
+
+    
+
+    filepath = sorted(glob.glob(f"{data_dir}/ising_L{L}_T*.npy"))         # formated string thats why brackets
+                                                                          # just list of names
+    X =[]
+    y=[]
+
+    for files in filepath:
+        T= float(files.split("_T")[-1].replace(".npy",""))         
+        configs = np.load(files)                                 # assigning labels as per the heading , 0 =ordered ,1 = disordered
+        X.append(configs)
+        y.append(np.full(configs.shape[0],T))
+
+    
+    X= np.concatenate(X,axis=0)    
+    y= np.concatenate(y,axis=0)
+
+    
+
+    X= (X+1)/2
+
+    X = X[...,np.newaxis]
+
+
+    # Shuffle the data
+    indices = np.arange(X.shape[0])
+    np.random.shuffle(indices)
+
+    X = X[indices]
+    y =y[indices]
+
+
+
+     # Split into train/test first, then split train into train/val
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        X, y, test_size=0.3, stratify=y, random_state=42
+    )
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=42
+    )
+
+    print(f" Loaded {X.shape[0]} samples")
+    print(f"Train: {X_train.shape},Tval: {X_val.shape}, Test: {X_test.shape}")
+    print(f"Train: {y_train.shape},Tval: {y_val.shape}, Test: {y_test.shape}")
+    print(f"Class balance: Ordered={np.sum(y==0)}, Disordered={np.sum(y==1)}")
+
+    np.savez("../preprocessed_data_reg/train_data.npz", X_train = X_train, y_train = y_train)
+    np.savez("../preprocessed_data_reg/val_data.npz", X_val = X_val, y_val = y_val)
+    np.savez("../preprocessed_data_reg/test_data.npz", X_test = X_test, y_test = y_test)
+
+    
+
+if __name__ == "__main__":
+    load_and_prepare_reg_data("../data")
---
+++ src/train_cnn_temp_reg.py
@@ -0,0 +1,127 @@
+"""
+train_cnn_phase_classifier.py
+-----------------------------
+This module defines and trains a Convolutional Neural Network (CNN) to classify 
+2D Ising model spin configurations into ordered and disordered phases.
+
+The model is trained using datasets prepared by the `load_and_prepare_phase_data` 
+function, which provides normalized lattice configurations labeled according 
+to the critical temperature (T_c).
+
+Key features:
+- CNN architecture for binary phase classification.
+- Includes dropout regularization and early stopping.
+- Automatically saves the best-performing model (based on validation loss) into `best_model/best_cnn_model.h5`.
+- Saves training history for later visualization and analysis into `history/cnn_training_history.npy`..
+
+Author - Amal Sebastian
+Date - October 2025
+
+"""
+
+
+
+
+
+
+import numpy as np
+from preprocess_data import load_and_prepare_phase_data
+import tensorflow as tf 
+from tensorflow.keras import layers,models,callbacks
+from tensorflow.keras.optimizers import Adam
+import os
+
+
+def build_cnn():
+    """
+    Build a simple Convolutional Neural Network (CNN) for binary phase classification.
+    
+    Returns
+    -------
+    model : tf.keras.Model
+        Compiled CNN model ready for training.
+    """
+    
+    # --------------------------------------------------------
+    # (a) Define input shape and layer structure
+    # --------------------------------------------------------
+
+
+    input_shape=(32,32,1)
+    
+    model =models.Sequential([
+        layers.Conv2D(32,(3,3),activation='relu',input_shape=input_shape),
+        layers.MaxPooling2D((2,2)),
+        layers.Conv2D(64,(2,2),activation='relu'),
+        layers.MaxPooling2D((2,2)),
+        layers.Conv2D(128, (3,3), activation='relu'),
+        layers.Flatten(),
+        layers.Dense(128,activation='relu'),
+        layers.Dropout(0.2),
+        layers.Dense(1,activation='linear')  # Binary classification (ordered/disordered)
+    ])
+
+    return model
+
+
+
+def train_model():
+    """
+    Train the CNN on Ising model configurations and save the best-performing model.
+    """
+    
+
+
+    model = build_cnn()
+
+    #X_train,X_val,X_test,y_train,y_val,y_test= load_and_prepare_phase_data("../data")
+
+    print("Reading in data...")
+    # Load the data from npz files
+    Train = np.load("../preprocessed_data_reg/train_data.npz")
+    X_train = Train['X_train']
+    y_train = Train['y_train']
+
+    Val = np.load("../preprocessed_data_reg/val_data.npz")
+    X_val = Val['X_val']
+    y_val = Val['y_val']
+
+
+    # checkpoint to save the best minimum  validation point
+    checkpoint_cb = callbacks.ModelCheckpoint(
+        filepath="best_model/best_cnn_model_reg.h5", save_best_only=True,save_weights_only= False,monitor="val_loss", mode="min", verbose=1
+    )
+
+    # stops if the validation doesnt improve over a patience range.
+   # earlystop_cb = callbacks.EarlyStopping(
+   #     monitor="val_loss", patience=50, restore_best_weights=True, verbose=1
+   # )
+
+    optimizer = Adam(learning_rate=1e-5)
+     
+    model.compile(optimizer=optimizer,
+                  loss='mse',
+                  metrics=['mae'])
+
+
+        # Train model
+    history = model.fit(
+        X_train, y_train,
+        epochs=300,
+        batch_size=32,
+        validation_data=(X_val, y_val),
+        callbacks=[checkpoint_cb],
+        verbose=1
+    )
+
+    # Save training history
+    np.save("history/cnn_training_history_reg.npy", history.history)
+
+    print(" Training complete. Best model saved as 'best_cnn_model_reg.h5'.")
+
+if __name__ == "__main__":
+    train_model()
+
+
+
+    
